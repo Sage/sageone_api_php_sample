b395de8702bea78ca8c106474367932a8e16623d
Ram Krishna Banstola <banstola@users.noreply.github.com>
2017-04-06 18:29:07 +0300

    [FIX]Make payload encode value null for get request
    Currently following line produces warning. This would remove that warning
    ``` 
          $encoded = base64_encode($this->request_body_params);
    ```

--- sageone_signer.php
+++ sageone_signer.php
@@ -114,7 +114,11 @@ class SageoneSigner {
 	/* base64 encode the request body */
 	private function encodedBody() {
 		$body = array();
-		$encoded = base64_encode($this->request_body_params);
+		$encode=null;
+		if(!is_array($this->request_body_params)){
+			
+			$encoded = base64_encode($this->request_body_params);
+		}
 		$body["body"] = $encoded;
 		return $body;
 	}
