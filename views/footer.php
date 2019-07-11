b3e0b1b97817e8b7bf2dfbfb686ae01d004f5f7e
Jon Burger <jon.burger@sage.com>
2019-07-11 15:27:36 +0100

    Design update

--- views/footer.php
+++ views/footer.php
@@ -1,41 +1,4 @@
-        <footer>
-          <div>Sign Up for Trial Account - Accounting:
-            <ul class="flags">
-              <li><a href="https://mysageone.ca.sageone.com/signup/new?product=accounts_extra" target="_blank">🇨🇦</a></li>
-              <li><a href="https://central.eu.sageone.com/signup/new?tier_code=SageOne-Accounting-DE&tier_rate_plan_code=Monthly&type=try&currency_code=EUR&locale=de-DE" target="_blank">🇩🇪</a></li>
-              <li><a href="https://central.eu.sageone.com/signup/new?tier_code=SageOne-Accounting-ES&tier_rate_plan_code=Monthly&type=try&currency_code=EUR&locale=es-ES" target="_blank">🇪🇦</a></li>
-              <li><a href="https://central.eu.sageone.com/signup/new?tier_code=SageOne-Accounting-FR&tier_rate_plan_code=Monthly&type=try&currency_code=EUR&locale=fr-FR" target="_blank">🇫🇷</a></li>
-              <li><a href="https://app.sageone.com/signup?product=accounting" target="_blank">🇬🇧/🇮🇪</a></li>
-              <li><a href="https://mysageone.na.sageone.com/signup/new?tier_code=SageOne-Accounting-US&tier_rate_plan_code=Monthly&currency_code=USD&type=try&locale=en-US" target="_blank">🇺🇸</a></li>
-            </ul>
-          </div>
-          <div>Sign Up for Trial Account - Start:
-            <ul class="flags">
-              <li><a href="https://mysageone.ca.sageone.com/signup/new?product=micro" target="_blank">🇨🇦</a></li>
-              <li><a href="https://central.eu.sageone.com/signup/new?tier_code=SageOne-Start-DE&tier_rate_plan_code=Monthly&type=try&currency_code=EUR&locale=de-DE" target="_blank">🇩🇪</a></li>
-              <li><a href="https://central.eu.sageone.com/signup/new?tier_code=SageOne-Start-ES&tier_rate_plan_code=Monthly&type=try&currency_code=EUR&locale=es-ES" target="_blank">🇪🇦</a></li>
-              <li><a href="https://central.eu.sageone.com/signup/new?tier_code=SageOne-Start-FR&tier_rate_plan_code=Monthly&type=try&currency_code=EUR&locale=fr-FR" target="_blank">🇫🇷</a></li>
-              <li><a href="https://app.sageone.com/signup?product=micro" target="_blank">🇬🇧/🇮🇪</a></li>
-              <li><a href="https://mysageone.na.sageone.com/signup/new?tier_code=SageOne-Start-US&tier_rate_plan_code=Monthly&currency_code=USD&type=try&locale=en-US" target="_blank">🇺🇸</a></li>
-            </ul>
-          </div>
-          <div>User Account Login:
-            <ul class="flags">
-              <li><a href="https://mysageone.ca.sageone.com/login?locale=en" target="_blank">🇨🇦</a></li>
-              <li><a href="https://central.eu.sageone.com/login?locale=de-DE" target="_blank">🇩🇪</a></li>
-              <li><a href="https://central.eu.sageone.com/login?locale=es-ES" target="_blank">🇪🇦</a></li>
-              <li><a href="https://central.eu.sageone.com/login?locale=fr-FR" target="_blank">🇫🇷</a></li>
-              <li><a href="https://app.sageone.com/login" target="_blank">🇬🇧/🇮🇪</a></li>
-              <li><a href="https://mysageone.na.sageone.com/signin" target="_blank">🇺🇸</a></li>
-            </ul>
-          </div>
-          <div>User Account Login:
-            <ul>
-              <li><a href="https://www.sageone.com/?clear" target="_blank" title="Show country selection (the 'flag' page) again">Reset Country Cookie</a></li>
-            </ul>
-          </div>
-        </footer>
-      </div>
+
     </div>
   </body>
 </html>
--- views/header.php
+++ views/header.php
@@ -1,20 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sage Accounting API Sample App - PHP</title>
+    <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap">
     <link type="text/css" rel="stylesheet" href="sample_app.css">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   </head>
   <body>
-    <header class="navbar navbar-fixed-top navbar-inverse">
-      <div class="container">
-        <a id="logo" href="/">Sage Accounting API Sample App - PHP</a>
-      </div>
-    </header>
-    <div class="container">
-      <div class="center well">
